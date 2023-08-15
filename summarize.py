bbba65b2392ac10d9e3eee83734f67be0de8d04e
Ubuntu <azureuser@A100-1.n30ihtx4ta0uhkwo220qflvbua.bx.internal.cloudapp.net>
2023-08-15 12:56:26 +0000

    update api

--- summarize.py
+++ summarize.py
@@ -10,7 +10,7 @@ import openai_config
 from langchain.chains.summarize import load_summarize_chain
 import nltk
 from langchain.docstore.document import Document
-import fitz
+# import fitz
 import re
 from docx2python import docx2python
 import chinese_converter
@@ -108,18 +108,10 @@ class ChineseSplitter(TextSplitter):
         return get_chunks(text)
 
 
-from omegaconf import OmegaConf
 
-if __name__ == "__main__":
-    cli_cfg = OmegaConf.from_cli()
-
-
-    llm = AzureChatOpenAI(temperature=0, deployment_name="gpt-4-32k")
-    text_splitter = ChineseSplitter()
-    file_dir = "./data/食品政策文档/"
-    abstract_dir =f"{file_dir}/abstracts/"
-    os.makedirs(abstract_dir, exist_ok=True)
-    product_name = cli_cfg.product
+def recall_rules_from_doc(all_rules, product_name, file_dir):
+    if product_name in all_rules: return all_rules
+    rules = ""
     for file_loc in list_files_recursive(file_dir):
         print(file_loc)
         ext = os.path.splitext(file_loc)[1]
@@ -129,16 +121,76 @@ if __name__ == "__main__":
         texts = text_splitter.split_text(text)
         print("size of texts: ", len(texts))
         docs = [Document(page_content=t) for t in texts]
-        prompt_template = f"请从如下描述中提取并总结出跟{product_name}相关的进出口和物品寄递政策。\n"
+        prompt_template = f"请从如下描述中提取并总结出跟{product_name}相关的进出口和物品寄递政策。如果没有相关的内容，则返回无。相关的政策可能是通用的规则，没有明确提及该商品名称。\n"
         prompt_template += """
         {text}
-        摘要为:"""
+        """ + f"摘要为："
         PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
         chain = load_summarize_chain(llm, chain_type="map_reduce", return_intermediate_steps=True, map_prompt=PROMPT, combine_prompt=PROMPT)
         abstract = chain({"input_documents": docs}, return_only_outputs=True)
-        print(abstract)
-        with open(f"{abstract_dir}/{file_name}_{product_name}_abstract.txt", "w") as f:
-            f.writelines(abstract["output_text"])
+        # print(abstract)
+        if abstract["output_text"] == "无": continue
+        else: rules += f"\n {abstract['output_text']}"
+    all_rules[product_name] = rules
+    return all_rules
 
+
+
+
+
+import sys
+import os
+import time
+from omegaconf import OmegaConf
+from datetime import datetime
+import pandas as pd
+from customGPT.gpt4custom import *
+
+
+if __name__ == "__main__":
+    cli_cfg = OmegaConf.from_cli()
+    llm = AzureChatOpenAI(temperature=0, deployment_name="gpt-4-32k", model="gpt-4-32k")
+    text_splitter = ChineseSplitter()
+    file_dir = "./data/食品政策文档/"
+    abstract_dir =f"{file_dir}/abstracts/"
+    os.makedirs(abstract_dir, exist_ok=True)
+
+    date_prefix = datetime.today().strftime('%m-%d-%H-%M')
+    rules_file_loc = f"data/extracted_rules.csv"
+    res_file_loc = f"data/gpt_res_{date_prefix}.csv"
+
+    question_file_loc = "../customGPT/data/关务食品类材料2023803/食品类收寄标准-0803.xlsx"
+    df_questions = pd.read_excel(question_file_loc, sheet_name="测试数据")
+    questions_list = [row.tolist() for _, row in df_questions[["物品名称", "寄件属性"]].drop_duplicates().iterrows()]
+
+    # questions_list = [["饼干","个人"]]
+
+    all_rules = {}
+    if os.path.exists(rules_file_loc):
+        df = pd.read_csv(rules_file_loc, encoding='utf-8', sep='\t')
+        all_rules = {row["Product"]: row["Rules"] for _, row in df.iterrows()}
+    res_list = []
+    q_r_list = []
+    policies_file_dir = "data/食品政策文档"
+    # product_name = cli_cfg.product
+    for i, (product_name, entity) in enumerate(questions_list):
+        food_prompt = get_food_prompt(product_name)
+        res = get_gpt_response(food_prompt).strip()
+        print(f"{i+1}/{len(questions_list)}", product_name, res)
+        time.sleep(1)
+        if res != "是": continue
+        all_rules = recall_rules_from_doc(all_rules, product_name, policies_file_dir)
+        rules = all_rules[product_name]
+        res_list.append([product_name, rules])
+        df = pd.DataFrame(data=res_list, columns=["Product", "Rules"])
+        df.to_csv(rules_file_loc, index=False, encoding='utf-8', sep='\t')
+        extra_info = query_gpt((product_name, entity))
+        time.sleep(1)
+        prompt = get_prompt((product_name, entity), rules, extra_info=extra_info)
+        res = get_gpt_response(prompt).strip()
+        q_r_list.append([product_name, entity, rules, prompt, res])
+        df = pd.DataFrame(q_r_list, columns=["Product", "Entity", "Rules", "Prompt", "GPT"])
+        df.to_csv(res_file_loc, index=False, encoding='utf-8', sep='\t')
+        time.sleep(1)
 
 
